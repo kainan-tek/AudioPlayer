9496cdf07326018555f92cadbd81ac8fc429c8d5
kainan <kainanos@outlook.com>
2025-04-21 12:10:52 +0800

    20250421

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,25 @@
 pluginManagement {
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/central")}
+        maven { url = uri("https://maven.aliyun.com/repository/public")}
+        maven { url = uri("https://maven.aliyun.com/repository/google")}
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
         google()
         mavenCentral()
-//        maven { url = uri("https://maven.aliyun.com/repository/central")}
-//        maven { url = uri("https://maven.aliyun.com/repository/public")}
-//        maven { url = uri("https://maven.aliyun.com/repository/google")}
-//        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
-//        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/central")}
+        maven { url = uri("https://maven.aliyun.com/repository/public")}
+        maven { url = uri("https://maven.aliyun.com/repository/google")}
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
+        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
         google()
         mavenCentral()
-//        maven { url = uri("https://maven.aliyun.com/repository/central")}
-//        maven { url = uri("https://maven.aliyun.com/repository/public")}
-//        maven { url = uri("https://maven.aliyun.com/repository/google")}
-//        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin")}
-//        maven { url = uri("https://maven.aliyun.com/repository/jcenter")}
     }
 }
 
